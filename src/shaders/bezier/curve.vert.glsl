de76e770d5ead8ba804ee79301c414f5dd5ca565
alejandrocalles <gabriel.jimenezcalles@epfl.ch>
2024-05-07 13:18:49 +0200

    Add shaders to display a 2d bezier curve for validation

---
+++ src/shaders/bezier/curve.vert.glsl
@@ -0,0 +1,36 @@
+precision highp float;
+
+vec3 bez4(vec3 a, vec3 b, vec3 c, vec3 d, float t) {
+    return mix(
+        mix(
+            mix(a, b, t),
+            mix(b, c, t),
+            t
+        ),
+        mix(
+            mix(b, c, t),
+            mix(c, d, t),
+            t
+        ),
+        t
+    );
+}
+
+#define NPOINTS 4
+
+uniform vec2 cpoints[NPOINTS];
+
+attribute float t;
+
+vec2 bez4_2d(float t) {
+    vec3 a = vec3(cpoints[0], 0.);
+    vec3 b = vec3(cpoints[1], 0.);
+    vec3 c = vec3(cpoints[2], 0.);
+    vec3 d = vec3(cpoints[3], 0.);
+    return bez4(a, b, c, d, t).xy;
+}
+
+void main() {
+    gl_PointSize = 40.;
+    gl_Position = vec4(bez4_2d(t), 0., 1.); 
+}
