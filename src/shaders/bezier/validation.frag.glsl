5b2f438228399816a24cc6b327bcf61b359c1562
alejandrocalles <gabriel.jimenezcalles@epfl.ch>
2024-05-12 19:10:44 +0200

    Add an index for the validation shaders.

--- src/shaders/bezier/validation.frag.glsl
+++ src/shaders/bezier/validation.frag.glsl
@@ -16,6 +16,13 @@ vec3 bez4(vec3 a, vec3 b, vec3 c, vec3 d, float t) {
     );
 }
 
+vec3 dbez4(vec3 a, vec3 b, vec3 c, vec3 d, float t) {
+	return normalize(
+        3. * pow(1. - t, 2.) * (b - a)
+        + 6. * (1. - t) * t * (c - b) + 3. * pow(t, 2.) * (d - c)
+    );
+}
+
 #define NPOINTS 4
 
 uniform vec2 cpoints[NPOINTS];
@@ -29,11 +36,18 @@ vec2 bez4_2d(float t) {
     vec3 d = vec3(cpoints[3], 0.);
     return bez4(a, b, c, d, t).xy;
 }
+vec2 dbez4_2d(float t) {
+    vec3 a = vec3(cpoints[0], 0.);
+    vec3 b = vec3(cpoints[1], 0.);
+    vec3 c = vec3(cpoints[2], 0.);
+    vec3 d = vec3(cpoints[3], 0.);
+    return dbez4(a, b, c, d, t).xy;
+}
 
-bool in_segment(vec2 a, vec2 b, vec2 x) {
+bool in_segment(vec2 a, vec2 b, vec2 x, float epsilon) {
     float crossproduct = (x.y - a.y) * (b.x - a.x) - (x.x - a.x) * (b.y - a.y);
 
-    if (abs(crossproduct) > 0.005) {
+    if (abs(crossproduct) > epsilon) {
         return false;
     }
 
@@ -56,6 +70,9 @@ varying vec2 pos;
 void main() {
     float radius = 0.02;
     vec3 pixel_color = vec3(0.);
+
+    vec2 current = bez4_2d(t);
+    vec2 target = current + 0.2 * dbez4_2d(t);
     // Check if this pixel is close enough to a control point
     for (int i = 0; i < NPOINTS; i++) {
         if (distance(cpoints[i], pos) <= radius) {
@@ -64,13 +81,17 @@ void main() {
     }
 
     for (int i = 1; i < NPOINTS; i++) {
-        if (in_segment(cpoints[i -1], cpoints[i], pos)) {
+        if (in_segment(cpoints[i -1], cpoints[i], pos, 0.005)) {
             pixel_color = vec3(1.0, 0.0, 0.0);
         }
     }
 
+    if (in_segment(current, target, pos, 0.0005)) {
+        pixel_color = vec3(0.0, 0.0, 1.0);
+    }
+
     // Check if this pixel is close enough to the focused point in the curve
-    if (distance(bez4_2d(t), pos) <= radius) {
+    if (distance(current, pos) <= radius) {
         pixel_color = vec3(0.0, 1.0, 0.0);
     }
     gl_FragColor = vec4(pixel_color, 1.);
